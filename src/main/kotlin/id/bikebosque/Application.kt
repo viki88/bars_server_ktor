435e7f13e4bf4e0bb1890f2acae350cf970b078b
telkomdev-viki <vikiandrianto88@gmail.com>
2023-06-15 23:06:34 +0700

    change host url

--- src/main/kotlin/id/bikebosque/Application.kt
+++ src/main/kotlin/id/bikebosque/Application.kt
@@ -7,7 +7,7 @@ import io.ktor.server.netty.*
 import org.ktorm.database.Database
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "barsserver-development.up.railway.app", module = Application::module)
         .start(wait = true)
 }
 
